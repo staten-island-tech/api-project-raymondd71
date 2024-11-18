0ee635ab659cf592603acee55b1a892c06317ebe
raymondd71 <raymondd71@nycstudents.net>
2024-11-18 12:32:35 -0500

    seqwaewewq

--- app/main.js
+++ app/main.js
@@ -1,1 +1,16 @@
 import "./style.css";
+async function getData() {
+  try {
+    const response = await fetch("https://valorant-api.com/v1/weapons");
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    console.log(error);
+    alert("sorry could not find that weapeon");
+  }
+}
+getData();
