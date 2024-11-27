c851bcdc6bc79fbd33f45d89dd2d45344f0ba411
raymondd71 <raymondd71@nycstudents.net>
2024-11-27 11:49:53 -0500

    zcxczxczx

--- app/JS/main.js
+++ app/JS/main.js
@@ -11,7 +11,7 @@ async function getData() {
     }
   } catch (error) {
     console.log(error);
-    alert("sorry could not find that weapon");
+    alert("something went wrong :(");
   }
 }
 getData();
@@ -22,11 +22,15 @@ function createCards(data) {
       "beforeend",
       `<div class="card w-[21%] h-[16rem] border-[5px] border-black flex flex-col items-center justify-around mb-[5%] text-center ">
       <h1>${thing.displayName} </h1>
-       <img class= "w-auto h-[200px] object-contain" src="${thing.displayIcon}" alt="${thing.displayIcon}" > </div>`
+       <img class= "w-auto h-[200px] object-contain" src="${thing.displayIcon}" alt="${thing.displayIcon}" ></div>`
     )
   );
 }
-
+document.querySelector("#rifle").addEventListener("click", async () => {
+  const rifleWeapons = data.filter(
+    (data) => weapon.shopData && weapon.shopData.category === "Rifle"
+  );
+});
 // async function getData(){
 //   data.forEach((x)=>{});
 //   createCards(data);
@@ -34,3 +38,4 @@ function createCards(data) {
 //   console.group(error);
 //   alert("sorry")
 // }
+//<h2> Fire Rate: ${thing.weaponStats.fireRate}</h2>
